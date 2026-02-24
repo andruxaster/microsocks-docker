3dc3cf7557d115be31bc28105d01f545bde0ee66
andruxaster <andruxaster@gmail.com>
2026-02-24 09:19:27 +0200

    Refactor entrypoint.sh to use environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-microsocks -i 0.0.0.0 -p 1080 -u $USER -P '$PASSWORD'
+set -eu
+
+LISTEN_ADDR="${LISTEN_ADDR:-0.0.0.0}"
+LISTEN_PORT="${LISTEN_PORT:-1080}"
+
+if [ -n "${PROXY_USER:-}" ] || [ -n "${PROXY_PASS:-}" ]; then
+  : "${PROXY_USER:?Set PROXY_USER}"
+  : "${PROXY_PASS:?Set PROXY_PASS}"
+  exec microsocks -i "$LISTEN_ADDR" -b 0.0.0.0 -p "$LISTEN_PORT" -u "$PROXY_USER" -P "$PROXY_PASS"
+else
+  exec microsocks -i "$LISTEN_ADDR" -b 0.0.0.0 -p "$LISTEN_PORT"
+fi
